650a97aeab31a5cb6ec5db4751b7050825d42a5e
sonders <778277514@qq.com>
2019-11-05 16:06:04 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,17 @@
 module.exports = {
-    baseUrl:"././",
-    productionSourceMap:false,
+    productionSourceMap: false,
     devServer: {
-
         proxy: {
-
             '/api': {
-
                 target: 'https://way.jd.com/jisuapi', //对应服务器地址
-
                 changeOrigin: true, //允许跨域
-
                 ws: true,
-
                 pathRewrite: {
-
                     '^/api': ''
-
                 }
-
             }
-
         }
-
-    }
-
+    },
+    publicPath:"./",
+    // 输出文件目录
 }
